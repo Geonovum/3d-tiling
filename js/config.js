2b54e39549089798a0d5f4f4e4b88442057d3882
Wilko Quak <w.quak@geonovum.nl>
2025-08-21 11:58:41 +0200

    publish date toegevoegd.

--- js/config.js
+++ js/config.js
@@ -35,7 +35,7 @@ let respecConfig = {
 
   //-- publishDate is verplicht -------------------------------------------------------
   //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
-  //publishDate: "2025-01-13",
+  publishDate: "2025-01-13",
   //eventueel is het mogelijk een versienummer mee te geven, maar bij Geonovum werken we gewoonlijk alleen met datum als onderdeel van de permanente URI.
   //publishVersion: "0.0.2",
   //previousVersion: "0.0.1",
